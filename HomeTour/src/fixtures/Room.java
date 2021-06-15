5bc3ea9f0ccf756b67aafda9e11f988fecfb44e2
Santiago Hincapie <santiagohr81@hotmail.com>
2021-06-15 00:53:41 -0400

    first commit

---
+++ HomeTour/src/fixtures/Room.java
@@ -0,0 +1,21 @@
+package fixtures;
+
+public class Room extends Fixtures{
+
+	public Room(String name, String shortDescription, String longDescription) {
+		super("The Foyer", "a small foyer", "The small entryway of a neo-colonial house. A dining room is open to the south where a large table");
+		
+		
+		
+	}
+
+	public Room() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+	
+
+}
